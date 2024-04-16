8198a4cc969f026f4d248ce053ec349778ed91f2
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2024-04-16 16:05:37 -0300

    api

--- WaterQuality.Api/Program.cs
+++ WaterQuality.Api/Program.cs
@@ -1,9 +1,34 @@
+using MongoDB.Driver;
+
+using WaterQuality.Context.MongoContext;
+using WaterQuality.Domain.Environment;
+using WaterQuality.Domain.Water;
+using WaterQuality.Interfaces.Context;
+using WaterQuality.Interfaces.Repository;
+using WaterQuality.Interfaces.Services;
+using WaterQuality.Repository.Environment;
+using WaterQuality.Repository.Water;
+using WaterQuality.Services.Environment;
+using WaterQuality.Services.Water;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IMongoDbContext>(sp =>
+{
+    var connectionString = "mongodb://192.168.0.65:27017";
+    var dbName = "WaterQuality";
+    return new MongoDbContext(connectionString, dbName);
+});
+
+builder.Services.AddScoped<IEnvironmentalRepository, EnvironmentalRepository>();
+builder.Services.AddScoped<IWaterQualityRepository, WaterQualityRepository>();
+builder.Services.AddScoped<IEnvironmentService, EnvironmentService>();
+builder.Services.AddScoped<IWaterService, WaterService>();
 
 WebApplication app = builder.Build();
 
@@ -16,29 +41,77 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-string[] summaries = new[]
+#region WATER QUALITY
+app.MapGet("/waterQuality", (IWaterService service) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return service.Get();
 })
-.WithName("GetWeatherForecast")
+.WithName("GetWaterQuality")
 .WithOpenApi();
 
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+app.MapGet("/currentWaterQuality", (IWaterService service) =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    return service.GetRealtime();
+})
+.WithName("GetWaterQuality")
+.WithOpenApi();
+
+app.MapGet("/currentDayWaterQuality", (IWaterService service) =>
+{
+    return service.GetCurrentDay();
+})
+.WithName("GetWaterQuality")
+.WithOpenApi();
+
+app.MapGet("/waterQuality/{date}", (IWaterService service, DateTime date) =>
+{
+    return service.GetFromDay(date);
+})
+.WithName("GetWaterQuality")
+.WithOpenApi();
+
+app.MapPost("/waterQuality/", (IWaterService service, WaterQualityParameter parameter) =>
+{
+    return service.Save(parameter);
+})
+.WithName("GetWaterQuality")
+.WithOpenApi();
+#endregion
+#region ENVIRONMENT
+app.MapGet("/environmentQuality", (IEnvironmentService service) =>
+{
+    return service.Get();
+})
+.WithName("GetenvironmentQuality")
+.WithOpenApi();
+
+app.MapGet("/currentenvironmentQuality", (IEnvironmentService service) =>
+{
+    return service.GetRealtime();
+})
+.WithName("GetenvironmentQuality")
+.WithOpenApi();
+
+app.MapGet("/currentDayenvironmentQuality", (IEnvironmentService service) =>
+{
+    return service.GetCurrentDay();
+})
+.WithName("GetenvironmentQuality")
+.WithOpenApi();
+
+app.MapGet("/environmentQuality/{date}", (IEnvironmentService service, DateTime date) =>
+{
+    return service.GetFromDay(date);
+})
+.WithName("GetenvironmentQuality")
+.WithOpenApi();
+
+app.MapPost("/environmentQuality/", (IEnvironmentService service, EnvironmentalParameter parameter) =>
+{
+    return service.Save(parameter);
+})
+.WithName("GetenvironmentQuality")
+.WithOpenApi();
+#endregion
+
+app.Run();
