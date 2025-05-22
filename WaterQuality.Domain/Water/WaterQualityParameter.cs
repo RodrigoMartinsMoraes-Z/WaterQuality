2ce721abe4735420a009690000f33c65e53c044d
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2025-05-22 09:47:52 -0300

    update tds to float

--- WaterQuality.Domain/Water/WaterQualityParameter.cs
+++ WaterQuality.Domain/Water/WaterQualityParameter.cs
@@ -1,2 +1,2 @@
 ﻿namespace WaterQuality.Domain.Water;
-public record WaterQualityParameter(int Tds = 0, decimal Temperature = 0, decimal Ph = 0, DateTime Date = default);
+public record WaterQualityParameter(float Tds = 0, decimal Temperature = 0, decimal Ph = 0, DateTime Date = default);
